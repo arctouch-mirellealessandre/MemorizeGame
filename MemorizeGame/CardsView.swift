862001f963a1f07709ce37320f8540b5d22c5ea3
Mirelle Alessandre <mirelle.alessandre@arctouch.com>
2024-04-24 10:29:55 -0300

    Refactor CardsView with .modifier(Cardify())

--- MemorizeGame/CardsView.swift
+++ MemorizeGame/CardsView.swift
@@ -1,69 +1,60 @@
 import SwiftUI
 
 struct CardsView: View {
-    typealias Card = MemoryGame<String>.Card
-
-    let card: Card
-    
-    init(_ card: Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        ZStack {
-			let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-				Pie(endAngle: .degrees(240))
-					.opacity(Constants.Pie.opacity)
-					.overlay(                
-						Text(card.content)
-							.font(.system(size: Constants.FontSize.largest))
-							.minimumScaleFactor(Constants.FontSize.scaleFactor)
-							.multilineTextAlignment(.center)
-							.aspectRatio(1, contentMode: .fit)
-							.padding(Constants.Pie.inset)
-					)
-					 .padding(Constants.inset)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            base.fill()
-            .opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
-    }
-}
-
-private struct Constants {
-	static let cornerRadius: CGFloat = 12
-	static let lineWidth: CGFloat = 2
-	static let inset: CGFloat = 5
+	typealias Card = MemoryGame<String>.Card
 	
-	struct FontSize {
-		static let largest: CGFloat = 200
-		static let smallest: CGFloat = 10
-		static let scaleFactor: CGFloat = smallest / largest
+	let card: Card
+	
+	init(_ card: Card) {
+		self.card = card
 	}
 	
-	struct Pie {
-		static let opacity: CGFloat = 0.5
-		static let inset: CGFloat = 0.5
+	var body: some View {
+		ZStack {
+			Pie(endAngle: .degrees(240))
+				.opacity(Constants.Pie.opacity)
+				.overlay(
+					Text(card.content)
+						.font(.system(size: Constants.FontSize.largest))
+						.minimumScaleFactor(Constants.FontSize.scaleFactor)
+						.multilineTextAlignment(.center)
+						.aspectRatio(1, contentMode: .fit)
+						.padding(Constants.Pie.inset)
+				)
+				.padding(Constants.inset)
+				.modifier(Cardify(isFaceUp: card.isFaceUp))
+				.opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+		}
+	}
+	
+	private struct Constants {
+		static let inset: CGFloat = 5
+		struct FontSize {
+			static let largest: CGFloat = 200
+			static let smallest: CGFloat = 10
+			static let scaleFactor: CGFloat = smallest / largest
+		}
+		
+		struct Pie {
+			static let opacity: CGFloat = 0.5
+			static let inset: CGFloat = 0.5
+		}
 	}
 }
 
 #Preview {
-  VStack {
-    HStack {
-      CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "X", id: "test1"))
-      CardsView(MemoryGame<String>.Card(content: "X", id: "test1"))
-        .aspectRatio(4/3, contentMode: .fit)
-    }
-    HStack {
-      CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "This is a very long string and I hope it fits", id: "test1"))
-      CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "X", id: "test1"))
-    }
-  }
-  .padding()
-  .foregroundStyle(.green)
+	VStack {
+		HStack {
+			CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "X", id: "test1"))
+			CardsView(MemoryGame<String>.Card(content: "X", id: "test1"))
+				.aspectRatio(4/3, contentMode: .fit)
+		}
+		HStack {
+			CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "This is a very long string and I hope it fits", id: "test1"))
+			CardsView(MemoryGame<String>.Card(isFaceUp: true, content: "X", id: "test1"))
+		}
+	}
+	.padding()
+	.foregroundStyle(.green)
 }
+
